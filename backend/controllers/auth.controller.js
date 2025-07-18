43a7c1a5f938285a9020e816f5526ce03362e85c
Dharmesh patel <93909226+Dharmeshp1582@users.noreply.github.com>
2025-07-18 19:57:58 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -32,8 +32,8 @@ export const signUp = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-      sameSite: "strict",
-      secure: false, // set to true in production with HTTPS
+      sameSite: "None",
+      secure: true, // set to true in production with HTTPS
     });
 
     return res.status(201).json({
@@ -69,8 +69,8 @@ export const Login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "strict",
-      secure: false,
+      sameSite: "None",
+      secure: true,
     });
 
     return res.status(200).json({
